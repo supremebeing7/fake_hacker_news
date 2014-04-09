2665642024151e69a68a84cdcff4a08aa992a175
Mark Lehman & Brentwood Reid <student@epicodus.com>
2014-04-08 18:33:18 -0700

    fix factorygirl for comments

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,15 +1,15 @@
 FactoryGirl.define do
-  factory :link, :class => Link do
+  factory :link do
     description 'IE6 is finally dead'
     url 'http://microsoft.com'
     vote_count 1
     user_name 'frankie23131'
   end
 
-  factory :comment, class: Comment do
+  factory :comment do
     content 'This post is total bs.  I have been using IE for a long time and it is the best browserevery invented.  Bill Gates is a genius.  Steve Jobs is a jerk and all you Apple users can suck it!!!!'
     user_name 'b_gates'
     commentable_id 4
-    commentable_type Link
+    commentable_type 'Link'
   end
 end
